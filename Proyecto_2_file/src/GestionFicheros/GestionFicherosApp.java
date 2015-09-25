19f07d77d3b3b0f68b84ebd0077b3606c4f14f76
tonymateo <tonymateo95@gmail.com>
2015-09-25 19:41:06 +0200

    Ejercicio 2 a falta de el metodo cambiar nombre

---
+++ Proyecto_2_file/src/GestionFicheros/GestionFicherosApp.java
@@ -0,0 +1,13 @@
+package GestionFicheros;
+
+import gestionficheros.MainGUI;
+
+public class GestionFicherosApp {
+
+	public static void main(String[] args) {
+		GestionFicherosImpl getFicherosImpl = new GestionFicherosImpl();
+		new MainGUI(getFicherosImpl).setVisible(true);
+
+	}
+
+}
