858a9c265662101f087facde3d8a4ebae3be5351
tonymateo <tonymateo95@gmail.com>
2015-10-01 10:39:00 +0200

    nuevos metodos creados pero no lee todas las lineas

--- Streams/src/Streams/Metodos.java
+++ Streams/src/Streams/Metodos.java
@@ -6,37 +6,64 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Metodos {
+	public String texto = null;
+	public String texto2 = null;
 	
 	public boolean compararContenido(File fichero1, File fichero2) throws IOException{
-		boolean repite=true;
-		while (repite==true){
+			if (fichero1.canRead()==true && fichero2.canRead()==true){
+				//comparo lo que he leido de los 2 ficheros
+				if (leyendoF1(fichero1).equalsIgnoreCase(leyendoF2(fichero2))){
+					System.out.println("1º FICHERO");
+					System.out.println(leyendoF1(fichero1));
+					System.out.println("2º FICHERO");
+					System.out.println(leyendoF2(fichero2));
+					System.out.println("Son iguales");
+				}else{
+					System.out.println("1º FICHERO");
+					System.out.println(leyendoF1(fichero1));
+					System.out.println("2º FICHERO");
+					System.out.println(leyendoF2(fichero2));
+					System.out.println("NO Son iguales");
+					}
+				}else{
+					System.out.println("No va");
+					}
+			return true;
+		}
+	//método que me lee el primer fichero y me devuelve lo que ha leido
+	public String leyendoF1(File fichero1) throws IOException{
 		FileReader fr = new FileReader(Streams.fichero1);
-		FileReader fr2 = new FileReader(Streams.fichero2);
-		
+
 		BufferedReader bfr = new BufferedReader(fr);
+		
+		while (bfr.readLine()!=null){
+			//leo la primera linea y me la guardo en la variable texto
+			texto = bfr.readLine();
+				if (bfr.readLine()==null){
+					System.out.println("este es el texto que me guardo del 1"+texto);
+					break;
+				}
+			}
+		bfr.close();
+		return texto;
+		
+	}
+	//método que me lee el segundo fichero y me devuelve el string que ha leido
+	public String leyendoF2(File fichero2) throws IOException{
+		FileReader fr2 = new FileReader(Streams.fichero2);
+	
 		BufferedReader bfr2 = new BufferedReader(fr2);
 		
-		String texto= bfr.readLine();
-		String texto2= bfr2.readLine();
-		
-		System.out.println("1º FICHERO");
-		System.out.println(texto);
-		System.out.println("2º FICHERO");
-		System.out.println(texto2);
-		
-		if (fichero1.canRead()==false && fichero1.canRead()==false){
-			if (texto.equalsIgnoreCase(texto2)){
-				System.out.println("Son iguales");
-				bfr.close();
-				bfr2.close();
-			}else{
-				System.out.println("NO Son iguales");
-				repite=false;
+		while (bfr2.readLine()!=null){
+			//leo el segundo documento y me lo guardo en la variable texto2
+			texto2 = bfr2.readLine();
+				if (bfr2.readLine()==null){
+					System.out.println("este es el texto que me guardo del 2"+texto2);
+					break;
 				}
-			}else{
-				repite=false;
 			}
-		}
-		return true;
+		bfr2.close();
+		return texto2;
+		
 	}
 }
