72961fcea8c2ce8ca278319ab0a407e9a25acd85
tonymateo <tonymateo95@gmail.com>
2015-10-05 08:50:32 +0200

    ejercicio 1c parte 1 con excepciones

--- Streams/src/Streams/Metodos.java
+++ Streams/src/Streams/Metodos.java
@@ -6,30 +6,42 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Metodos {
+
 	public boolean compararContenido(File fichero1, File fichero2) throws IOException{
 		
-		FileReader fr = new FileReader(Streams.fichero1);
-		FileReader fr2 = new FileReader(Streams.fichero2);
 		
-		BufferedReader bfr = new BufferedReader(fr);
-		BufferedReader bfr2 = new BufferedReader(fr2);
+		if (fichero1.canRead() && fichero1.canWrite() && fichero1.exists() && fichero2.canRead() && fichero2.canWrite() && fichero2.exists()){
+			
+			FileReader fr = new FileReader(Streams.fichero1);
+			FileReader fr2 = new FileReader(Streams.fichero2);
 		
-		//me guardo el contenido de la primera linea del fichero entrada.txt
-		String texto = bfr.readLine();
-		//me guardo el contenido de la primera linea del fichero entrada1.txt
-		String texto2 = bfr2.readLine();
+			BufferedReader bfr = new BufferedReader(fr);
+			BufferedReader bfr2 = new BufferedReader(fr2);
 		
-		//compruebo que las 2 primeras lineas de ambos ficheros sean iguales, si lo son paso a la siguiente y compruebo de nuevo
-		while ((texto != null) || (texto2 != null)){
+			//me guardo el contenido de la primera linea del fichero entrada.txt
+			String texto = bfr.readLine();
+			//me guardo el contenido de la primera linea del fichero entrada1.txt
+			String texto2 = bfr2.readLine();
+		
+			//compruebo que las 2 primeras lineas de ambos ficheros sean iguales, si lo son paso a la siguiente y compruebo de nuevo
+			while ((texto != null) || (texto2 != null)){
 				if(texto.equals(texto2)){
 					texto = bfr.readLine();
 					texto2 = bfr2.readLine();
 				}else{
 					return false;
 				}
+			}
+			bfr.close();
+			bfr2.close();
+		}else{
+			throw new IOException("Algún fichero no tiene permisos de escritura o de lectura, o el fichero no existe");
 		}
-		bfr.close();
-		bfr2.close();
-		return true;
-		}
+			return true;
+	}
+	public int buscarPalabra (File fichero1, String palabra, boolean primera_aparicion){
+		return 0;
+		
+	}
+
 }
