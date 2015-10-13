41f8381f6f6251942361e379f68fb99576a45a34
tonymateo <tonymateo95@gmail.com>
2015-10-13 11:55:13 +0200

    añadiendo algunas excepciones

--- Streams/src/Streams/Metodos.java
+++ Streams/src/Streams/Metodos.java
@@ -14,75 +14,79 @@ public class Metodos {
 	///////
 	public boolean compararContenido(File fichero1, File fichero2) throws IOException{
 		if (fichero1.canRead() && fichero1.canWrite() && fichero1.exists() && fichero2.canRead() && fichero2.canWrite() && fichero2.exists()){
-			
-			FileReader fr = new FileReader(Streams.fichero1);
-			FileReader fr2 = new FileReader(Streams.fichero2);
+			try{
+				FileReader fr = new FileReader(Streams.fichero1);
+				FileReader fr2 = new FileReader(Streams.fichero2);
 		
-			BufferedReader bfr = new BufferedReader(fr);
-			BufferedReader bfr2 = new BufferedReader(fr2);
+				BufferedReader bfr = new BufferedReader(fr);
+				BufferedReader bfr2 = new BufferedReader(fr2);
 		
-			//me guardo el contenido de la primera linea del fichero entrada.txt
-			String texto = bfr.readLine();
-			//me guardo el contenido de la primera linea del fichero entrada1.txt
-			String texto2 = bfr2.readLine();
+				//me guardo el contenido de la primera linea del fichero entrada.txt
+				String texto = bfr.readLine();
+				//me guardo el contenido de la primera linea del fichero entrada1.txt
+				String texto2 = bfr2.readLine();
 		
-			//compruebo que las 2 primeras lineas de ambos ficheros sean iguales, si lo son paso a la siguiente y compruebo de nuevo
-			while ((texto != null) || (texto2 != null)){
-				if(texto.equals(texto2)){
-					texto = bfr.readLine();
-					texto2 = bfr2.readLine();
-				}else{
-					return false;
+				//compruebo que las 2 primeras lineas de ambos ficheros sean iguales, si lo son paso a la siguiente y compruebo de nuevo
+				while ((texto != null) || (texto2 != null)){
+					if(texto.equals(texto2)){
+						texto = bfr.readLine();
+						texto2 = bfr2.readLine();
+					}else{
+						return false;
+					}
 				}
+				bfr.close();
+				bfr2.close();
+				}catch(FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}else{
+				throw new IOException("Algún fichero no tiene permisos de escritura o de lectura, o el fichero no existe");
 			}
-			bfr.close();
-			bfr2.close();
-		}else{
-			throw new IOException("Algún fichero no tiene permisos de escritura o de lectura, o el fichero no existe");
-		}
-			return true;
+		return true;
 	}
 	
 	//Toni Mateo
 	//Segunda parte Ejercicio 1c
 	///////
 	public int buscarPalabra (File ficheroParaBuscar, String palabraParaBuscar, boolean primera_aparicion) throws IOException{
-		try{
 			if(ficheroParaBuscar.exists() && ficheroParaBuscar.canRead()){
-				int contadorLineas=1;
-				FileReader fr = new FileReader(Streams.fichero1);
+				try {
+					int contadorLineas=1;
+					FileReader fr;
+					fr = new FileReader(Streams.fichero1);
 		
-				BufferedReader bfr = new BufferedReader(fr);
+					BufferedReader bfr = new BufferedReader(fr);
 		
-				String texto = bfr.readLine();
+					String texto = bfr.readLine();
 		
-				while(texto != null){
-					if(texto.equals(palabraParaBuscar)){
-						return contadorLineas;
-					}else{
-						texto = bfr.readLine();
-						contadorLineas++;	
+					while(texto != null){
+						if(texto.equals(palabraParaBuscar)){
+							return contadorLineas;
+						}else{
+							texto = bfr.readLine();
+							contadorLineas++;	
+						}
 					}
-				}
-			}else{
+					}catch(FileNotFoundException e) {
+						e.printStackTrace();
+					}
+				}else{
 				throw new IOException("No se ha encontrado el fichero");
 			}
-		}catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	return -1;
 	}
 	
+	//Toni Mateo
+	//Ejercicio 1d
+	///////
 	public void ordenarFichero(File Origen, File Destino, int tipo_orden) throws FileNotFoundException{
 		BufferedReader bfr = new BufferedReader(new FileReader(Streams.ficheroOrigen));
-		
 		try {
 			String texto = bfr.readLine();
 			ArrayList<String> textoParaOrdenar = null;
 			textoParaOrdenar.add(texto);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
