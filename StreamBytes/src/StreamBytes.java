ea3222dc6b8e90213133edb47f78793cc933b696
tonymateo <tonymateo95@gmail.com>
2015-10-16 18:31:32 +0200

    ejercicio 1e acabado

--- StreamBytes/src/StreamBytes.java
+++ StreamBytes/src/StreamBytes.java
@@ -1,24 +1,24 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Scanner;
 
 
 public class StreamBytes {
 	
+	//Toni Mateo 
+	//ejercicio 1e
+	
 	static String usuarioOrigen = "";
 	static String usuarioDestino = "";
 	public static String comillas = "\"";
 	static int contador = 0;
-	//String rutaFicheroOrigen = "C:\Users\2dam\Desktop\lena.jpg";//Ruta al fichero de origen
-    //String rutaFicheroDestino = "destino.txt"; //Ruta al fichero de destino
 	
+	//pruebas C:\Users\2dam\Desktop\lena.jpg
 	public static void main(String[] args){
 		abrirFichero();
 		CopiaFicheroBinario();
@@ -32,13 +32,15 @@ public class StreamBytes {
 		//me creo un string vacio que luego se rellenará con la palabra que el usuario desee buscar
 		Scanner entrada = new Scanner (System.in); //Creación de un objeto Scanner
 		usuarioOrigen = entrada.nextLine (); //Invoco un método sobre un objeto Scanner
-		
-		System.out.println("Indica el fichero destino");
+
+		System.out.println("Indica la ruta donde quieres que se copie el fichero anterior"+"\n"+
+				"(CUIDADO: si no se espicifica ruta, el fichero se copiará en la carpeta de este proyecto)");
 		//me creo un string vacio que luego se rellenará con la palabra que el usuario desee buscar
 		Scanner salida = new Scanner (System.in); //Creación de un objeto Scanner
 		usuarioDestino = salida.nextLine (); //Invoco un método sobre un objeto Scanner
 		
 		CopiaFicheroBinario();
+		//hago una comprobación final para ver si la copia se ha realizado con exito o no
 		if(usuarioDestino!=null){
 			System.out.println("Copia realizada con éxito");
 			System.out.println("He copiado un total de "+comillas+contador+comillas+" bytes");
@@ -50,24 +52,24 @@ public class StreamBytes {
 	public static void CopiaFicheroBinario (){  
 		
 		try{
-			
-		File ficheroOrigen = new File(usuarioOrigen);//Creamos objeto de tipo fichero con la ruta origen que nos proporciona el usuario.
-	    File ficheroDestino = new File(usuarioDestino);//Creamos objeto de tipo fichero con la ruta destino que nos proporciona el usuario.
+			File ficheroOrigen = new File(usuarioOrigen);
+			File ficheroDestino = new File(usuarioDestino);
 	    
-	    //me creo 2 objetos tipo FileInputStream para luego poder usar el buffered
-	    FileInputStream fr = new FileInputStream(ficheroOrigen);
-	    FileOutputStream fw = new FileOutputStream(ficheroDestino);
+			//me creo 2 objetos tipo FileInputStream para luego poder usar el buffered
+			FileInputStream fr = new FileInputStream(ficheroOrigen);
+			FileOutputStream fw = new FileOutputStream(ficheroDestino);
 	    
-	    BufferedInputStream leerFicheroOrigen = new BufferedInputStream(fr);
-	    BufferedOutputStream escribirFicheroDestio = new BufferedOutputStream(fw);
+			BufferedInputStream leerFicheroOrigen = new BufferedInputStream(fr);
+			BufferedOutputStream escribirFicheroDestio = new BufferedOutputStream(fw);
 	        
-	            int copiaEnBytes;//variable donde me guardo los bytes copiados
-	            while((copiaEnBytes=(leerFicheroOrigen).read()) != -1){ //mientras no llegue al final del fichero...(-1)
-	                escribirFicheroDestio.write(copiaEnBytes);//los bytes que tengo guardados en la variable anterior las escribo en el fichero destino
-	                contador++;
-	            }
-	            leerFicheroOrigen.close();
-	            escribirFicheroDestio.close();
+			int copiaEnBytes;//variable donde me guardo los bytes copiados
+	            
+			while((copiaEnBytes=(leerFicheroOrigen).read()) != -1){ //mientras no llegue al final del fichero...(-1)
+				escribirFicheroDestio.write(copiaEnBytes);//los bytes que tengo guardados en la variable anterior las escribo en el fichero destino
+				contador++;
+			}
+			leerFicheroOrigen.close();
+			escribirFicheroDestio.close();
 	        }
 	        catch(FileNotFoundException e){
 	            e.printStackTrace();//si no encuentra el archivo, lanzo una excepcion
